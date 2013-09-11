ba04465c0a9f0a07e88d99ddacef76357b8efa12
Mike Clark <mike@mike-clark.org>
2013-09-11 15:56:02 -0400

    added some knockout.js stuff
    Signed-off-by: Mike Clark <mike@mike-clark.org>

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+function AppViewModel() {
+    this.firstName = ko.observable("Bert");
+    this.lastName = ko.observable("Bertington");
+
+    this.fullName = ko.computed(function() {
+        return this.firstName() + " " + this.lastName();    
+    }, this);
+
+
+    this.goToPage = function(page) { 
+        this.chosenPageId(page);
+        $.get('/mail', { folder: folder }, self.chosenFolderData);
+    };
+}
+
+ko.applyBindings(new AppViewModel());
 
